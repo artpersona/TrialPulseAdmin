eb4b0f0cf59d2e4cbda78d5f1380257c40891640
jperez1738 <jp11perez@gmail.com>
2022-07-05 20:50:35 +0800

    fixed footer

--- components/Footer.js
+++ components/Footer.js
@@ -3,8 +3,13 @@ import styles from "../styles/Footer.module.css";
 export default function Footer() {
   return (
     <footer>
-      <div className="flex-shrink-0 py-3 bg-dark text-white-50">
-        <p className={styles.footer__text}>Trial Pulse ©2022</p>
+      <div
+        className={[
+          "flex-shrink-0 py-3 bg-dark text-white-50",
+          styles.container,
+        ].join(" ")}
+      >
+        <h7 className={styles.footer__text}>Trial Pulse ©2022</h7>
       </div>
     </footer>
   );
